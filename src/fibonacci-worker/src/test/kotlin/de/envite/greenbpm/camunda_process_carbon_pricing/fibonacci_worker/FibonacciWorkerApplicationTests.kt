ad9c34c7b9d3cce6debe2d4fe160fa514adda8c7
Luc Weinbrecht <luc@weinbrecht.de>
2024-06-21 13:37:42 +0200

    fixed tests by adding spring zeebe test to have a zeebe in the spring context available

--- src/fibonacci-worker/src/test/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/fibonacci_worker/FibonacciWorkerApplicationTests.kt
+++ src/fibonacci-worker/src/test/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/fibonacci_worker/FibonacciWorkerApplicationTests.kt
@@ -1,9 +1,11 @@
 package de.envite.greenbpm.camunda_process_carbon_pricing.fibonacci_worker
 
+import io.camunda.zeebe.spring.test.ZeebeSpringTest
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@ZeebeSpringTest
 class FibonacciWorkerApplicationTests {
 
 	@Test
