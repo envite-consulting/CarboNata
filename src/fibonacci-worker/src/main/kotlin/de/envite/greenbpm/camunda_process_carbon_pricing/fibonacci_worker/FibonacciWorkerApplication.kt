b1b6ac80aadd00a9d659093856afd194d7e2b50c
Luc Weinbrecht <luc@weinbrecht.de>
2024-06-21 10:06:35 +0200

    deploying model via zeebe worker

--- src/fibonacci-worker/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/fibonacci_worker/FibonacciWorkerApplication.kt
+++ src/fibonacci-worker/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/fibonacci_worker/FibonacciWorkerApplication.kt
@@ -1,9 +1,11 @@
 package de.envite.greenbpm.camunda_process_carbon_pricing.fibonacci_worker
 
+import io.camunda.zeebe.spring.client.annotation.Deployment
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@Deployment(resources = ["classpath*:/bpmn/**/*.bpmn"])
 class FibonacciWorkerApplication
 
 fun main(args: Array<String>) {
