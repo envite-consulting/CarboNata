3122f8c1a32df9bbc5ff64de4fc6acf56a890c3a
Luc Weinbrecht <luc@weinbrecht.de>
2024-06-28 11:09:10 +0200

    added spring boot app which will export prometheus data to calculate costs and push it to camunda optimize variable ingestion api

---
+++ src/cabonataplate/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/cabonataplate/CabonataplateApplication.kt
@@ -0,0 +1,11 @@
+package de.envite.greenbpm.camunda_process_carbon_pricing.cabonataplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CabonataplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<CabonataplateApplication>(*args)
+}
