a59803dec61326d59abe641ae8623dc4b00884f1
Sebastian Guelpen <sebastian.guelpen@suedleasing.de>
2024-08-03 23:56:33 +0200

    add optimize ingestion client

--- src/carbonataplate/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/carbonataplate/usecase/out/CostCommand.kt
+++ src/carbonataplate/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/carbonataplate/usecase/out/CostCommand.kt
@@ -1,4 +1,8 @@
 package de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.usecase.out
 
+import de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.domain.model.output.CamundaOptimizeIngestion
+import reactor.core.publisher.Mono
+
 interface CostCommand {
+    fun exportCost(ingestionValue: CamundaOptimizeIngestion): Mono<String>
 }
