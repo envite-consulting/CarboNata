08a65c8b6b033e81ab382680ac326205f204bdfe
CRoberto1926 <3205022+CRoberto1926@users.noreply.github.com>
2024-08-12 18:47:33 +0200

    Refactorings for Camunda Optimize Ingestion API Client

--- src/carbonataplate/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/carbonataplate/adapter/camundaoptimize/CamundaOptimizeClient.kt
+++ src/carbonataplate/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/carbonataplate/adapter/camundaoptimize/CamundaOptimizeClient.kt
@@ -1,33 +1,19 @@
 package de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.adapter.camundaoptimize
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.adapter.camundaoptimize.config.OptimizeIngestionProperties
 import de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.domain.model.output.CamundaOptimizeIngestion
 import de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.usecase.out.CostCommand
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @Service
 class CamundaOptimizeClient(
-    private val objectMapper: ObjectMapper,
-    private val webClient: WebClient,
-    private val camundaIngestionProperties: OptimizeIngestionProperties
+    private val webClient: WebClient
 ) : CostCommand {
 
-    companion object {
-        private const val CAMUNDA_OPTIMIZE_CLIENT_REGISTRATION_ID = "carbonata-plate-client"
-    }
-
     override fun exportCost(ingestionValue: CamundaOptimizeIngestion): Mono<String> {
-        return webClient.post()
-            .uri("${camundaIngestionProperties.basePath}/api/ingestion/variable")
-            .attributes(clientRegistrationId(CAMUNDA_OPTIMIZE_CLIENT_REGISTRATION_ID))
-            .bodyValue(objectMapper.writeValueAsString(ingestionValue))
-            .retrieve()
-            .bodyToMono(String::class.java)
-            .doOnNext { response -> println("Response: $response") }
+        return webClient.post().uri("/api/ingestion/variable").bodyValue(ingestionValue).retrieve()
+            .bodyToMono(String::class.java).doOnNext { response -> println("Response: $response") }
             .doOnError { error -> println("Error: ${error.message}") }
     }
 }
