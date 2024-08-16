afcf8f51b55173889c51a5c68a938a199a3b151c
Luc Weinbrecht <luc@weinbrecht.de>
2024-08-16 08:26:47 +0200

    added logger

--- src/carbonataplate/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/carbonataplate/adapter/camundaoptimize/CamundaOptimizeClient.kt
+++ src/carbonataplate/src/main/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/carbonataplate/adapter/camundaoptimize/CamundaOptimizeClient.kt
@@ -2,6 +2,8 @@ package de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.adapter
 
 import de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.domain.model.ProcessReport
 import de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.usecase.out.CostCommand
+import de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.utils.Logging
+import de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate.utils.log
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
@@ -10,13 +12,13 @@ import org.springframework.web.reactive.function.client.WebClient
 class CamundaOptimizeClient(
     @Qualifier("optimizeIngestionWebClient")
     private val webClient: WebClient
-) : CostCommand {
+) : CostCommand, Logging {
 
     override fun exportCost(processReport: ProcessReport) {
         val ingestionData = mapProcessReport(processReport)
         webClient.post().uri("/api/ingestion/variable").bodyValue(ingestionData).retrieve()
-            .bodyToMono(String::class.java).doOnNext { response -> println("Response: $response") }
-            .doOnError { error -> println("Error: ${error.message}") }
+            .bodyToMono(String::class.java).doOnNext { response -> log().debug("Response: $response") }
+            .doOnError { error -> log().error("Error: ${error.message}") }
             .block()
     }
 }
