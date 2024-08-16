e9daced7cbf62401b6a39d96cfb5785f0c4f6ba1
Luc Weinbrecht <luc@weinbrecht.de>
2024-08-16 08:09:42 +0200

    cleanup application yaml

--- src/carbonataplate/src/test/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/carbonataplate/CarbonataplateApplicationTests.kt
+++ src/carbonataplate/src/test/kotlin/de/envite/greenbpm/camunda_process_carbon_pricing/carbonataplate/CarbonataplateApplicationTests.kt
@@ -2,8 +2,10 @@ package de.envite.greenbpm.camunda_process_carbon_pricing.carbonataplate
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CarbonataplateApplicationTests {
 
 	@Test
