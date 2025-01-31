15a3fb1b0581161b68c96d5b9fce216d6e3a4349
Maximilian Behrendt <maximilian.behrendt@envite.de>
2025-01-31 15:16:07 +0100

    use env variables for fibonacci worker c8 connection

--- start_locally.sh
+++ start_locally.sh
@@ -1,4 +1,16 @@
-# sh
+#!/bin/sh
+
+echo "sourcing .env-File"
+echo ""
+set -a
+. ./.env
+set +a
+
+if [ -z "${TF_VAR_CAMUNDA_CLUSTER_ID}" ];
+then
+    echo "You must set your local variable in the .env-File"
+    echo ""
+fi
 
 echo "Navigating to the corresponding directory (tf/cd)"
 cd tf/c8 || exit
@@ -11,7 +23,6 @@ echo "Applying terraform"
 echo ""
 terraform apply -auto-approve
 
-export GRAFANA_FORWORDED_PORT=9102
 echo "Forward Grafana Port to $GRAFANA_FORWORDED_PORT"
 echo ""
-kubectl port-forward -n grafana svc/kube-prometheus-stack-grafana $GRAFANA_FORWORDED_PORT:80
+kubectl port-forward -n grafana svc/kube-prometheus-stack-grafana $GRAFANA_FORWORDED_PORT:80 &> /dev/null &
--- tf/c8/modules/worker/variables.tf
+++ tf/c8/modules/worker/variables.tf
@@ -7,3 +7,28 @@ variable "create_module" {
   type        = bool
   default     = true
 }
+
+variable "CAMUNDA_CLUSTER_REGION" {
+  description = "Camunda Cluster Region"
+  type        = string
+}
+
+variable "CAMUNDA_CLUSTER_ID" {
+  description = "Camunda Cluster ID"
+  type        = string
+}
+
+variable "CAMUNDA_CLIENT_ID" {
+  description = "Camunda Client ID"
+  type        = string
+}
+
+variable "CAMUNDA_CLIENT_SECRET" {
+  description = "Camunda Client Secret"
+  type        = string
+}
+
+variable "CAMUNDA_CLIENT_MODE" {
+  description = "Camunda Client Mode"
+  type        = string
+}
